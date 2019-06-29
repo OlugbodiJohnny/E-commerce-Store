0e96c45dc13e1c2e178caf6d3f23767031ae29ed
voja <voja@gmail.com>
2019-06-29 14:19:02 +0200

    13

---
+++ src/main/java/com/vojislavk/cmsshoppingcart/controllers/AdminPagesController.java
@@ -0,0 +1,16 @@
+package com.vojislavk.cmsshoppingcart.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/pages")
+public class AdminPagesController {
+
+    @GetMapping
+    public String index() {
+        return "admin/pages/index";
+    }
+    
+}
