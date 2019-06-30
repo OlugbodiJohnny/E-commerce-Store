086e28f83ac0c16bdb316ec28e61e4e1d694e046
voja <voja@gmail.com>
2019-06-30 10:16:42 +0200

    20

--- src/main/java/com/vojislavk/cmsshoppingcart/controllers/AdminPagesController.java
+++ src/main/java/com/vojislavk/cmsshoppingcart/controllers/AdminPagesController.java
@@ -13,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -73,6 +74,17 @@ public class AdminPagesController {
         }
 
         return "redirect:/admin/pages/add";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable int id, Model model) {
+
+        Page page = pageRepo.getOne(id);
+
+        model.addAttribute("page", page);
+
+        return "admin/pages/edit";
+        
     }
     
 }
