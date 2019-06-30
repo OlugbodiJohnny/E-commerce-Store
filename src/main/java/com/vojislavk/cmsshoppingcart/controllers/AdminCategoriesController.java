d82219951b08ffcd61a304763743a022db30d5d5
voja <voja@gmail.com>
2019-06-30 17:49:58 +0200

    26

--- src/main/java/com/vojislavk/cmsshoppingcart/controllers/AdminCategoriesController.java
+++ src/main/java/com/vojislavk/cmsshoppingcart/controllers/AdminCategoriesController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -26,6 +27,17 @@ public class AdminCategoriesController {
         model.addAttribute("categories", categories);
 
         return "admin/categories/index";
+    }
+
+    // @ModelAttribute("category")
+    // public Category getCategory() {
+    //     return new Category();
+    // }
+
+    @GetMapping("/add")
+    public String add(Category category) {
+        
+        return "admin/categories/add";
     }
     
 }
