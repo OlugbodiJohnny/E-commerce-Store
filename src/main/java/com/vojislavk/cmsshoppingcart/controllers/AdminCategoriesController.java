13c205eb00eb0e45c6d3e5afd265e17ac42a792b
voja <voja@gmail.com>
2019-06-30 17:30:19 +0200

    25

---
+++ src/main/java/com/vojislavk/cmsshoppingcart/controllers/AdminCategoriesController.java
@@ -0,0 +1,31 @@
+package com.vojislavk.cmsshoppingcart.controllers;
+
+import java.util.List;
+
+import com.vojislavk.cmsshoppingcart.models.CategoryRepository;
+import com.vojislavk.cmsshoppingcart.models.data.Category;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/categories")
+public class AdminCategoriesController {
+
+    @Autowired
+    private CategoryRepository categoryRepo;
+
+    @GetMapping
+    public String index(Model model) {
+        
+        List<Category> categories = categoryRepo.findAll();
+
+        model.addAttribute("categories", categories);
+
+        return "admin/categories/index";
+    }
+    
+}
