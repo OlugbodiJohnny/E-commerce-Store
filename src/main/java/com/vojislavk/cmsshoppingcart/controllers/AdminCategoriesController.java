722df72c34f359c8f9bbe3b9ea3efa6fc4db3997
voja <voja@gmail.com>
2019-07-01 08:04:30 +0200

    28

--- src/main/java/com/vojislavk/cmsshoppingcart/controllers/AdminCategoriesController.java
+++ src/main/java/com/vojislavk/cmsshoppingcart/controllers/AdminCategoriesController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -71,6 +72,17 @@ public class AdminCategoriesController {
         }
 
         return "redirect:/admin/categories/add";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable int id, Model model) {
+
+        Category category = categoryRepo.getOne(id);
+
+        model.addAttribute("category", category);
+
+        return "admin/categories/edit";
+        
     }
     
 }
