98985eba95fa10bf848ed6915edde912f6a3f416
voja <voja@gmail.com>
2019-07-01 09:46:57 +0200

    32

---
+++ src/main/java/com/vojislavk/cmsshoppingcart/models/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.vojislavk.cmsshoppingcart.models;
+
+import com.vojislavk.cmsshoppingcart.models.data.Product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    
+}
