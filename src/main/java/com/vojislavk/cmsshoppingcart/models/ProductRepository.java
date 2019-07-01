feb4eb0617d304732cc675a4d25f3e4594ad7f9b
voja <voja@gmail.com>
2019-07-01 11:30:02 +0200

    35

--- src/main/java/com/vojislavk/cmsshoppingcart/models/ProductRepository.java
+++ src/main/java/com/vojislavk/cmsshoppingcart/models/ProductRepository.java
@@ -6,5 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
+	Product findBySlug(String slug);
+
     
 }
