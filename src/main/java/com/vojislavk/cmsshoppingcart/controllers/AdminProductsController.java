d31a08e79c8078f0fccb94a1d82dd5e32bd3365d
voja <voja@gmail.com>
2019-07-01 11:04:51 +0200

    34

--- src/main/java/com/vojislavk/cmsshoppingcart/controllers/AdminProductsController.java
+++ src/main/java/com/vojislavk/cmsshoppingcart/controllers/AdminProductsController.java
@@ -2,7 +2,9 @@ package com.vojislavk.cmsshoppingcart.controllers;
 
 import java.util.List;
 
+import com.vojislavk.cmsshoppingcart.models.CategoryRepository;
 import com.vojislavk.cmsshoppingcart.models.ProductRepository;
+import com.vojislavk.cmsshoppingcart.models.data.Category;
 import com.vojislavk.cmsshoppingcart.models.data.Product;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,16 +18,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdminProductsController {
 
     @Autowired
-    private ProductRepository productRepository;
+    private ProductRepository productRepo;
+
+    @Autowired
+    private CategoryRepository categoryRepo;
 
     @GetMapping
     public String index(Model model) {
         
-        List<Product> products = productRepository.findAll();
+        List<Product> products = productRepo.findAll();
 
         model.addAttribute("products", products);
 
         return "admin/products/index";
+    }
+
+    @GetMapping("/add")
+    public String add(Product product, Model model) {
+        
+        List<Category> categories = categoryRepo.findAll();
+        model.addAttribute("categories", categories);
+
+        return "admin/products/add";
     }
 
     
