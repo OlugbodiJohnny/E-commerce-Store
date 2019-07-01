77f8ede97141ac19e7f7d2f909e41eb4201320dc
voja <voja@gmail.com>
2019-07-01 08:48:20 +0200

    31

--- src/main/java/com/vojislavk/cmsshoppingcart/controllers/AdminCategoriesController.java
+++ src/main/java/com/vojislavk/cmsshoppingcart/controllers/AdminCategoriesController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -27,7 +29,7 @@ public class AdminCategoriesController {
     @GetMapping
     public String index(Model model) {
         
-        List<Category> categories = categoryRepo.findAll();
+        List<Category> categories = categoryRepo.findAllByOrderBySortingAsc();
 
         model.addAttribute("categories", categories);
 
@@ -125,6 +127,22 @@ public class AdminCategoriesController {
 
         return "redirect:/admin/categories";
         
+    }
+
+    @PostMapping("/reorder")
+    public @ResponseBody String reorder(@RequestParam("id[]") int[] id) {
+        
+        int count = 1;
+        Category category;
+
+        for (int categoryId : id) {
+            category = categoryRepo.getOne(categoryId);
+            category.setSorting(count);
+            categoryRepo.save(category);
+            count++;
+        }
+
+        return "ok";
     }
     
 }
--- src/main/java/com/vojislavk/cmsshoppingcart/models/CategoryRepository.java
+++ src/main/java/com/vojislavk/cmsshoppingcart/models/CategoryRepository.java
@@ -1,5 +1,7 @@
 package com.vojislavk.cmsshoppingcart.models;
 
+import java.util.List;
+
 import com.vojislavk.cmsshoppingcart.models.data.Category;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
     Category findByName(String name);
+
+    List<Category> findAllByOrderBySortingAsc();
     
 }
