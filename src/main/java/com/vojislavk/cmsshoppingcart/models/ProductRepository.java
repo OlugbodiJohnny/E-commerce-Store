cfea275cb9937fbef41fce65a837fd2108af9f48
voja <voja@gmail.com>
2019-07-03 08:13:11 +0200

    45

--- src/main/java/com/vojislavk/cmsshoppingcart/models/ProductRepository.java
+++ src/main/java/com/vojislavk/cmsshoppingcart/models/ProductRepository.java
@@ -2,13 +2,17 @@ package com.vojislavk.cmsshoppingcart.models;
 
 import com.vojislavk.cmsshoppingcart.models.data.Product;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
 	Product findBySlug(String slug);
 
-	Product findBySlugAndIdNot(String slug, int id);
+    Product findBySlugAndIdNot(String slug, int id);
+    
+    Page<Product> findAll(Pageable pagable);
 
     
 }
