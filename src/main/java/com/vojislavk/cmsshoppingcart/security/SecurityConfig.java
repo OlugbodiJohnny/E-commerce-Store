b8ae3db8f4e1540baf961e85eac45d5fefa47292
voja <voja@gmail.com>
2019-07-09 08:15:11 +0200

    68

---
+++ src/main/java/com/vojislavk/cmsshoppingcart/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.vojislavk.cmsshoppingcart.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/**").hasAnyRole("USER");
+
+        // http
+        //     .authorizeRequests()
+        //         .antMatchers("/category/**").access("hasRole('ROLE_USER')")
+        //         .antMatchers("/").access("permitAll");
+    }
+    
+}
