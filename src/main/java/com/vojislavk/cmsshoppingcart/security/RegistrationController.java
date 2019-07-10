661908207e3df49c7ccfc0a3ba77250bcc60cb9c
voja <voja@gmail.com>
2019-07-10 09:06:32 +0200

    73

--- src/main/java/com/vojislavk/cmsshoppingcart/security/RegistrationController.java
+++ src/main/java/com/vojislavk/cmsshoppingcart/security/RegistrationController.java
@@ -1,12 +1,17 @@
 package com.vojislavk.cmsshoppingcart.security;
 
+import javax.validation.Valid;
+
 import com.vojislavk.cmsshoppingcart.models.UserRepository;
 import com.vojislavk.cmsshoppingcart.models.data.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -22,5 +27,23 @@ public class RegistrationController {
     @GetMapping
     public String register(User user) {
         return "register";
+    }
+
+    @PostMapping
+    public String register(@Valid User user, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "register";
+        }
+
+        if (! user.getPassword().equals(user.getConfirmPassword())) {
+            model.addAttribute("passwordMatchProblem", "Passwords do not match!");
+            return "register";
+        }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepo.save(user);
+
+        return "redirect:/login";
     }
 }
